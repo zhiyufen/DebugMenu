6d4a900458a1a3c74333c7121fde170374e5311b
zhiyufen <steven_zhi@sina.com>
2019-07-02 22:32:28 +0800

    [Setup] 构建DebugMenu的SDK及Demo模块
    [#Issue] None
    [#Solution] None
    
    Signed-off-by: zhiyufen <steven_zhi@sina.com>

---
+++ app/src/main/java/com/android/zhiyufen/debugmenu/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.zhiyufen.debugmenu.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mebugmenu/src/main/java/com/android/zhiyufen/mebugmenu/activity/DebugMenuSettingsActivity.java
@@ -0,0 +1,6 @@
+package com.android.zhiyufen.mebugmenu.activity;
+
+import android.app.Activity;
+
+public class DebugMenuSettingsActivity extends Activity {
+}
